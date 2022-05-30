ae57912444a14babbb2e4b4fc3a2d6b4e1664c4f
swapnilbarve <100339918+swapnilbarve@users.noreply.github.com>
2022-05-30 19:54:38 +0530

    Add files via upload

---
+++ exam.js
@@ -0,0 +1,45 @@
+console.log('working');
+
+// interview question
+
+// 1) difference between == and ===
+// ans :- both are comparison operator 
+// ==    compare value
+// ===  compare value and datatype
+
+
+
+
+// 2) what is spread operator
+// ans :- the spread operator is the new addition to the set of operator in javascript es6
+//         it take iterable and expand it into individual Element.apply
+
+
+
+// 3) difference between var, let and const
+// ans:- var is global scope and it can be ressigned and redeclare it can be modify later.
+
+//      let is block scope and it can be ressigned and redeclare it can be modify later.
+
+//      const is also  block scope it cannot be ressigned and redeclare it cannot be modify later.
+
+
+
+
+//  4) what is execution context?
+ 
+//  ans :- the browser javascript engine create a environment to handle the transformation and execution 
+//     of the javascript code this environment is known as execution context.Element
+
+
+
+
+// 5) what is meant by first class function?
+
+// ans:- a programming language is said to have first class function when function in that language
+//     are treated like any other variable.
+
+
+
+// 6)     what is closure ?
+// ans :- closure is combination of function bundle together with ref to its surrounding State
